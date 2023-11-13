326cc8ce723179471d4d79cbae6d9b6a99041c19
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2023-11-13 16:47:55 -0300

    Primeiras aplicações com angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./main/main.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, MainComponent]
 })
 export class AppComponent {
-  title = 'example-app';
+  mostrarComprados = true;
+
+  list = [
+    { produto: 'arroz', quantidade : 2, comprado: false },
+    { produto: 'leite', quantidade : 8, comprado: false },
+    { produto: 'nescau', quantidade : 1, comprado: false },
+  ]
+
+  markCheckbox(event: any, produto: string)
+  {
+	this.list.map(it => {
+		if (it.produto == produto)
+			it.comprado = event.target.checked;
+	});
+  }
+
+  showComprado(event: any)
+  {
+	this.mostrarComprados = !event.target.checked;
+  }
+
+  deleteItem(produto: string)
+  {
+	this.list = this.list.filter((item) => item.produto != produto);
+  }
 }
